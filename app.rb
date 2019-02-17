92d0928c41d1fbff8fddc4278ae68c4061443096
Priscilla H <priscillamhuynh@gmail.com>
2019-02-17 13:07:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   post '/checkout' do
     @session = session
     item = params["item"]
-    @sessions[:item] = item
+    @session[:item] = item
   end
   
   post '/checkout' do
